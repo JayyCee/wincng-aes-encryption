1018fe9cd0b6c3f69be6e9bebe2b21ff03304aba
=jc <=jc.at.cloud>
2023-04-03 02:34:50 -0400

    removed rgbIV[]
    Signed-off-by: =jc <=jc.at.cloud>

--- wincng_crypt_lib/wincng_aeskey.c
+++ wincng_crypt_lib/wincng_aeskey.c
@@ -20,12 +20,6 @@
 
 static const char *wincng_get_ntstat_s_by_v(NTSTATUS v);
 
-static const BYTE rgbIV[] =
-{
-	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
-	0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F
-};
-
 
 wincng_aes_ctx_t wincng_aes_ctx_new(const unsigned char *shared_secret_key, size_t shared_secret_key_size)
 {
